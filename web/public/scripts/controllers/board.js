647ec273348175ea0a42c52c17eb04567367f6d1
Cygnut <lee.crosby@outlook.com>
2017-01-20 02:20:55 +0000

    =

--- web/public/scripts/controllers/board.js
+++ web/public/scripts/controllers/board.js
@@ -7,8 +7,6 @@ function($scope, cardboardService, modalService) {
 	
 	const modalTemplateUrl = '/../../partials/card-modal.html';
 	
-	$scope.cards = cardboardService.cardboard.cards;
-	
 	$scope.addCard = function() {
 		modalService.showModal({
 			templateUrl: modalTemplateUrl
@@ -22,29 +20,82 @@ function($scope, cardboardService, modalService) {
 		});
 	}
 	
-	$scope.editCard = function(c) {
-		modalService.showModal({
-			templateUrl: modalTemplateUrl
-		}, {
-			modalTitle: 'Edit Card',
-			id: c.id,
-			left: c.left,
-			top: c.top,
-			title: c.title,
-			content: c.content
-		})
-		.then(function(result) {
-			cardboardService.updateCard(result);
-		});
+	function getSelectedCard()
+	{
+		return _.find($scope.cards, function(c) { return c.id === $scope.selectedCardId; });
+	}
+	
+	$scope.editCard = function() {
+		var c = getSelectedCard();
+		if (c)
+		{
+			modalService.showModal({
+				templateUrl: modalTemplateUrl
+			}, {
+				modalTitle: 'Edit Card',
+				id: c.id,
+				left: c.left,
+				top: c.top,
+				title: c.title,
+				content: c.content
+			})
+			.then(function(result) {
+				cardboardService.updateCard(result);
+			});
+		}
+	}
+	
+	$scope.deleteCard = function() {
+		var c = getSelectedCard();
+		if (c)
+		{
+			modalService.showModal({
+				templateUrl: '/../../partials/delete-card-modal.html'
+			}, {})
+			.then(function(result) {
+				cardboardService.deleteCard(c.id);
+			});
+		}
 	}
 	
 	$scope.onMoveStop = function(attrs, left, top) {
-		
 		cardboardService.updateCard({
 			id: parseInt(attrs.cardId),
 			left: left,
 			top: top
 		});
-		
 	}
+	
+	$scope.cardClick = function(card) {
+		if ($scope.editMode) {
+			// If clicking on an already selected card, then unselect it.
+			if (card.id === $scope.selectedCardId)
+				$scope.selectedCardId = null;
+			else
+				// If another card is clicked on, then select it.
+				$scope.selectedCardId = card.id;
+		}
+	}
+	
+	$scope.editClick = function() {
+		if ($scope.editMode)
+		{
+			$scope.selectedCardId = null;
+			// Leaving edit mode.
+			// Remove any ui highlighting and selected booleans.
+		}
+		else
+		{
+			$scope.selectedCardId = null;
+			// Entering edit mode.
+			// Ensure nothing selected.
+		}
+		
+		$scope.editMode = !$scope.editMode;
+	}
+	
+	$scope.selectedCardId = null;
+	$scope.editMode = false;
+	$scope.cards = cardboardService.cardboard.cards;
+	
 }]);
