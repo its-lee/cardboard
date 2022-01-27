19746d0846c8347143f9b6bbd0ebf850a19ea24c
Lee Crosby <ligand@hotmail.co.uk>
2022-01-27 20:36:26 +0000

    Update xxport.js

--- scripts/controllers/xxport.js
+++ scripts/controllers/xxport.js
@@ -2,7 +2,7 @@ angular.module('cardboard').controller('xxport', ['$scope', 'cardboardService',
 function($scope, cardboardService) {
     
     $scope.exportData = function() {
-        saveTextAs(JSON.stringify(cardboardService.cardboard), 'cardboard-' + Date.now().toString());
+        saveTextAs(JSON.stringify(cardboardService.cardboard), 'cardboard-' + Date.now().toString() + '.json');
     }
     
     $scope.importData = function(a, b, c) {
